b21095a9b600adf0acb7abeb9de5281f0d36627e
Will Hafner <whafner43@gmail.com>
2021-11-04 11:24:59 +0000

    Finished classwork exercise 1

--- test_times.py
+++ test_times.py
@@ -27,4 +27,13 @@ def test_just_overlapping_input():
 
     assert result == expected
 
+def test_several_intervals_each():
+    range1 = time_range("2010-01-12 10:00:00", "2010-01-12 10:40:00", 3, 300) # 10:00 -> 10:10, 10:15 -> 10:25, 10:30 -> 10:40
+    range2 = time_range("2010-01-12 10:05:00", "2010-01-12 10:45:00", 3, 300) # 10:05 -> 10:15, 10:20 -> 10:30, 10:35 -> 10:45
+
+    result = compute_overlap_time(range1, range2)
+    expected = [("2010-01-12 10:05:00", "2010-01-12 10:10:00"), ("2010-01-12 10:20:00", "2010-01-12 10:25:00"), ("2010-01-12 10:35:00", "2010-01-12 10:40:00")]
+
+    assert result == expected
+
     
